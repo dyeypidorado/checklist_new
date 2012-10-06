060eedafb4c069d7334bdd318ec0bbccaafaceb1
dyeypidorado <dyeypidorado@dyeypidorado-U-100.(none)>
2012-10-07 00:07:01 +0800

    Added Rails Admin

--- checklist_new/config/routes.rb
+++ checklist_new/config/routes.rb
@@ -1,4 +1,6 @@
 ChecklistNew::Application.routes.draw do
+  mount RailsAdmin::Engine => '/cms', :as => 'rails_admin'
+
   devise_for :admins
   root to: 'home#index'
 end
--- checklist_new/db/schema.rb
+++ checklist_new/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121006143104) do
+ActiveRecord::Schema.define(:version => 20121006153719) do
 
   create_table "admins", :id => false, :force => true do |t|
     t.string   "email",                            :default => "", :null => false
@@ -28,5 +28,18 @@ ActiveRecord::Schema.define(:version => 20121006143104) do
   end
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
 end
