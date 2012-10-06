283af22654b22793532402831220b6492ea5ed41
dyeypidorado <dyeypidorado@dyeypidorado-U-100.(none)>
2012-10-06 22:29:12 +0800

    Initial Commit

---
+++ checklist_new/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChecklistNew::Application.config.secret_token = 'e233c270619e3997b9cb100482ecc83bedd3c9366ed585a1e3a9e327ba70289413ae929455c28939f439b2362288c5d3c8b17dd80d8b9b26cfdd84dca730242e'
